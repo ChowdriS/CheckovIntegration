1bfbf4cde13bfbd52d8f395e2f960b327bd8c71d
Chowdri Sakthivel <114232036+ChowdriS@users.noreply.github.com>
2025-12-09 14:52:23 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -8,6 +8,16 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
